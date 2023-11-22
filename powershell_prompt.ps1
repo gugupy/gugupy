43ac0036381308421c1a79d3488ebb672c38143d
Gughanathan M <gugu.ap900@gmail.com>
2023-11-22 18:16:04 +0530

    Create powershell_prompt.ps1

---
+++ powershell_prompt.ps1
@@ -0,0 +1,36 @@
+function prompt {
+
+  $ESC = [char]27
+  $RED = "$ESC[91m"
+  $NC = "$ESC[00m"
+
+  $prompt_ = ""
+
+  # Current Directory
+  if ($(Get-Location).Path -eq $env:USERPROFILE) {
+    $prompt_ += "📂 ~"  
+  } else {
+    $path_c = $(Get-Location).Path.Split('\')[-1]
+    $prompt_ += "📂 " + $path_c 
+  }
+
+  # Identify Git Repo
+  $branch = (git branch) 2> ~/.error.log
+
+  if ($null -ne $branch) {
+    $prompt_ += "$RED git$NC($(git branch --show-current))"
+  }
+
+  # Localtime
+  $local_time = Get-Date -Format "HH:mm:ss"
+  $prompt_ += " [$local_time] "
+
+  # Battery Percentage Warning
+  $bat_per = (Get-CimInstance -ClassName Win32_Battery).EstimatedChargeRemaining
+  if ($bat_per -lt 30){
+    $prompt_ += "[🪫 $bat_per%]"
+  }
+
+  $prompt_ += "# "
+  $prompt_ 
+}
